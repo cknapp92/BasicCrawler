af8dc559caad2e17c4ce00ab1921e385c35414da
Conrad Knapp <conrad.knapp@bc.edu>
2017-08-03 20:21:51 -0400

    product name and price, crateandbarrel

--- untitled.py
+++ untitled.py
@@ -1,17 +1,29 @@
+from lxml import html
+import requests
 
-
-class BasicCrawler: 
+class AppCrawler: 
 
 	def __init__ (self, starting_url, depth):
 		self.starting_url = starting_url
 		self.depth = depth
+		self.apps = []
 
-	def crawl (self):
+	def crawl(self):
+		self.get_app_from_link(self.starting_url)
 		return
 
 	def get_app_from_link(self, link):
-		return
+		start_page = requests.get(link)
+		tree = html.fromstring(start_page.text)
 
+		name = tree.xpath('//div[@id="bodyWrap"]//h1[@id="productNameHeader"]/text()')
+		price = tree.xpath('//div[@id="bodyWrap"]//span[@class="regPrice"]/text()')[0]
+
+
+		print (name)
+		print (price)
+
+		return
 
 class App:
 
@@ -26,4 +38,8 @@ class App:
 		"\r\nDeveloper: " + self.developer.encode('UTF-8') +
 		"\r\nPrice: " + self.price.encode('UTF-8') + "\r\n")
 
-crawler = Crawler()
+crawler = AppCrawler('https://www.crateandbarrel.com/stirrup-antique-bronze-table-lamp/s215483?lkhd_id=xkyYCp&lkhd_type=fallback', 0)
+crawler.crawl()
+
+for app in crawler.apps:
+	print (app)
